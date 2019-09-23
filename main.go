29975c9b7b93796e8b8baf2bcdbc0fb44c7978b3
John Naylor <jonaylor89@gmail.com>
2019-09-23 14:50:42 -0400

    Add states and clock speed

--- main.go
+++ main.go
@@ -3,8 +3,54 @@ package main
 
 import (
   "fmt"
+  "time"
 )
 
+const (
+
+  // Process States
+
+  // CREATED : process created
+  CREATED = iota
+
+  // RUNNING : process running
+  RUNNING
+
+  // WAITING : process waiting
+  WAITING
+
+  // BLOCKED : process blocked
+  BLOCKED
+
+  // TERMINATED : process terminated
+  TERMINATED
+)
+
+// Process : Running set of code
+type Process struct {
+  PID int;
+  state int;
+  runtime int;
+  memory int;
+}
+
 func main() {
-  fmt.Println("Hello World")
+
+  p := Process{ 
+    PID: 1, 
+    state: CREATED,
+    runtime: 300,
+    memory: 45,
+   }
+
+  for {
+
+    p.state = RUNNING
+    fmt.Println(p)
+    p.state = WAITING
+
+    time.Sleep(200 * time.Millisecond)
+
+  }
+
 }
