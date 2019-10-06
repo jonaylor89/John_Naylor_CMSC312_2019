fc3b023f5ffd96a0e23010489accc73f44a63dc5
John Naylor <jonaylor89@gmail.com>
2019-10-06 16:43:16 -0400

    Create go mod

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/jonaylor89/John_Naylor_CMSC312_2019/sched"
 )
 
 const (
@@ -45,102 +47,6 @@ var (
 type Instruction struct {
 	OpCode string
 	param  int
-}
-
-// Process : Running set of code
-type Process struct {
-	PID     int
-	Name    string
-	state   int
-	runtime int
-	memory  int
-}
-
-// Scheduler : Controller to schedule process to run
-type Scheduler struct {
-	inMsg     chan *Process
-	processes []*Process
-	instructions []Instruction
-}
-
-// Run : Start the schedule and process execution
-func (s *Scheduler) Run() {
-	for {
-
-		// Check for new processes to schedule
-		select {
-		case x, ok := <-s.inMsg:
-			if ok {
-				// New process ready to be executed
-				s.processes = append(s.processes, x)
-
-			} else {
-				// Channel is closed to execution must exit
-				return
-			}
-		default:
-			// No new processes
-			break
-		}
-
-		for i, curProc := range s.processes {
-			curProc.state = RUNNING
-
-			// I'm assuming this will get much more complex beyond just subtracting runtime
-			// Fortunately, as of now it is basic round robin execution
-			curProc.runtime -= TimeQuantum
-			time.Sleep(200 * time.Millisecond)
-
-			if curProc.runtime <= 0 {
-				s.processes = remove(s.processes, i)
-			} else {
-				curProc.state = WAITING
-			}
-
-		}
-
-	}
-}
-
-// CreateProc : create a new process correctly
-func CreateProc(name string, runtime int, mem int) *Process {
-
-	ProcNum++
-
-	return &Process{
-		PID:     ProcNum,
-		Name:    name,
-		state:   CREATED,
-		runtime: runtime,
-		memory:  mem,
-	}
-}
-
-func createRandomProcessFromTemplate(templateName string, instructions [][]string, ch chan *Process) {
-
-	totalRuntime := 0
-	for _, instruction := range instructions {
-		if len(instruction) < 2 {
-			continue
-		}
-
-		templateRuntime, err := strconv.Atoi(instruction[1])
-		if err != nil {
-			fmt.Println("Error converting runtime to int", err)
-		}
-
-		// Jitter values by +-10
-		templateRuntime += rand.Intn(20) - 10
-
-		if instruction[0] == "CALCULATE" {
-			totalRuntime += templateRuntime
-		}
-
-		instruction[1] = strconv.Itoa(templateRuntime)
-	}
-
-	p := CreateProc("From template: "+templateName, totalRuntime, rand.Intn(100)+1)
-	ch <- p
 }
 
 func shuffleInstructions(vals [][]string) {
---
+++ sched/sched.go
@@ -0,0 +1,96 @@
+
+
+// Process : Running set of code
+type Process struct {
+	PID     int
+	Name    string
+	state   int
+	runtime int
+	memory  int
+}
+
+// Scheduler : Controller to schedule process to run
+type Scheduler struct {
+	inMsg     chan *Process
+	processes []*Process
+}
+
+// Run : Start the schedule and process execution
+func (s *Scheduler) Run() {
+	for {
+
+		// Check for new processes to schedule
+		select {
+		case x, ok := <-s.inMsg:
+			if ok {
+				// New process ready to be executed
+				s.processes = append(s.processes, x)
+
+			} else {
+				// Channel is closed to execution must exit
+				return
+			}
+		default:
+			// No new processes
+			break
+		}
+
+		for i, curProc := range s.processes {
+			curProc.state = RUNNING
+
+			// I'm assuming this will get much more complex beyond just subtracting runtime
+			// Fortunately, as of now it is basic round robin execution
+			curProc.runtime -= TimeQuantum
+			time.Sleep(200 * time.Millisecond)
+
+			if curProc.runtime <= 0 {
+				s.processes = remove(s.processes, i)
+			} else {
+				curProc.state = WAITING
+			}
+
+		}
+
+	}
+}
+
+// CreateProc : create a new process correctly
+func CreateProc(name string, runtime int, mem int) *Process {
+
+	ProcNum++
+
+	return &Process{
+		PID:     ProcNum,
+		Name:    name,
+		state:   CREATED,
+		runtime: runtime,
+		memory:  mem,
+	}
+}
+
+func createRandomProcessFromTemplate(templateName string, instructions [][]string, ch chan *Process) {
+
+	totalRuntime := 0
+	for _, instruction := range instructions {
+		if len(instruction) < 2 {
+			continue
+		}
+
+		templateRuntime, err := strconv.Atoi(instruction[1])
+		if err != nil {
+			fmt.Println("Error converting runtime to int", err)
+		}
+
+		// Jitter values by +-10
+		templateRuntime += rand.Intn(20) - 10
+
+		if instruction[0] == "CALCULATE" {
+			totalRuntime += templateRuntime
+		}
+
+		instruction[1] = strconv.Itoa(templateRuntime)
+	}
+
+	p := CreateProc("From template: "+templateName, totalRuntime, rand.Intn(100)+1)
+	ch <- p
+}
