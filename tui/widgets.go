513ae622bea9027f28ad02aef31ce79806ce93a1
John Naylor <jonaylor89@gmail.com>
2019-12-04 22:51:03 -0500

    Make widgets global variables

--- tui/widgets.go
+++ tui/widgets.go
@@ -12,6 +12,17 @@ import (
 	"github.com/jonaylor89/John_Naylor_CMSC312_2019/kernel"
 )
 
+var (
+	p0 *widgets.Plot
+	p1 *widgets.Plot
+	p2 *widgets.Plot
+	p3 *widgets.Plot
+	p  *widgets.Paragraph
+	l  *widgets.List
+	l0  *widgets.List
+	grid *ui.Grid
+)
+
 func initWidgets() {
 	
 }
@@ -39,14 +50,14 @@ func Render(k *kernel.Kernel) {
 		return data
 	}()
 
-	p0 := widgets.NewPlot()
+	p0 = widgets.NewPlot()
 	p0.Title = "braille-mode Line Chart"
 	p0.Data = sinData
 	p0.SetRect(0, 0, 50, 15)
 	p0.AxesColor = ui.ColorWhite
 	p0.LineColors[0] = ui.ColorGreen
 
-	p1 := widgets.NewPlot()
+	p1 = widgets.NewPlot()
 	p1.Title = "dot-mode line Chart"
 	p1.Marker = widgets.MarkerDot
 	p1.Data = [][]float64{[]float64{1, 2, 3, 4, 5}}
@@ -56,7 +67,7 @@ func Render(k *kernel.Kernel) {
 	p1.LineColors[0] = ui.ColorYellow
 	p1.DrawDirection = widgets.DrawLeft
 
-	p2 := widgets.NewPlot()
+	p2 = widgets.NewPlot()
 	p2.Title = "dot-mode Scatter Plot"
 	p2.Marker = widgets.MarkerDot
 	p2.Data = make([][]float64, 2)
@@ -67,7 +78,7 @@ func Render(k *kernel.Kernel) {
 	p2.LineColors[0] = ui.ColorCyan
 	p2.PlotType = widgets.ScatterPlot
 
-	p3 := widgets.NewPlot()
+	p3 = widgets.NewPlot()
 	p3.Title = "braille-mode Scatter Plot"
 	p3.Data = make([][]float64, 2)
 	p3.Data[0] = []float64{1, 2, 3, 4, 5}
@@ -78,11 +89,11 @@ func Render(k *kernel.Kernel) {
 	p3.Marker = widgets.MarkerBraille
 	p3.PlotType = widgets.ScatterPlot
 
-	p := widgets.NewParagraph()
+	p = widgets.NewParagraph()
 	p.Text = "CMSC312 Operating System Simulator"
 	p.SetRect(0, 0, 25, 5)
 
-	l := widgets.NewList()
+	l = widgets.NewList()
 	l.Title = "Ready Processes"
 	l.Rows = Map(k.ReadyQ, func(p *kernel.Process) string {
 		return fmt.Sprintf("%#v", p)
@@ -91,7 +102,7 @@ func Render(k *kernel.Kernel) {
 	l.WrapText = false
 	l.SetRect(0, 0, 25, 8)
 
-	l0 := widgets.NewList()
+	l0 = widgets.NewList()
 	l0.Title = "Waiting Processes"
 	l0.Rows = Map(k.WaitingQ, func(p *kernel.Process) string {
 		return fmt.Sprintf("%#v", p)
@@ -100,7 +111,7 @@ func Render(k *kernel.Kernel) {
 	l0.WrapText = false
 	l0.SetRect(0, 0, 25, 8)
 
-	grid := ui.NewGrid()
+	grid = ui.NewGrid()
 
 	grid.Set(
 		ui.NewRow(1.0/3,
@@ -122,4 +133,12 @@ func Render(k *kernel.Kernel) {
 	grid.SetRect(0, 0, termWidth, termHeight)
 
 	ui.Render(grid)
+}
+
+func EventLoop() {
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
 }
