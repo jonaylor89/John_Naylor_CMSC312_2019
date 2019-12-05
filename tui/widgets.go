6313083df1555b51589a639341e56486d45445d8
John Naylor <jonaylor89@gmail.com>
2019-12-05 13:22:16 -0500

    resize window

--- tui/widgets.go
+++ tui/widgets.go
@@ -18,9 +18,6 @@ import (
 
 var (
 	p0 *widgets.Plot
-	p1 *widgets.Plot
-	p2 *widgets.Plot
-	p3 *widgets.Plot
 	p  *widgets.Paragraph
 	l  *widgets.List
 	l0  *widgets.List
@@ -57,43 +54,12 @@ func Render(k *kernel.Kernel) {
 	}()
 
 	p0 = widgets.NewPlot()
-	p0.Title = "braille-mode Line Chart"
+	p0.Title = "Memory Usage"
 	p0.Data = sinData
 	p0.SetRect(0, 0, 50, 15)
 	p0.AxesColor = ui.ColorWhite
 	p0.LineColors[0] = ui.ColorGreen
-
-	p1 = widgets.NewPlot()
-	p1.Title = "dot-mode line Chart"
-	p1.Marker = widgets.MarkerDot
-	p1.Data = [][]float64{[]float64{1, 2, 3, 4, 5}}
-	p1.SetRect(50, 0, 75, 10)
-	p1.DotMarkerRune = '+'
-	p1.AxesColor = ui.ColorWhite
-	p1.LineColors[0] = ui.ColorYellow
-	p1.DrawDirection = widgets.DrawLeft
-
-	p2 = widgets.NewPlot()
-	p2.Title = "dot-mode Scatter Plot"
-	p2.Marker = widgets.MarkerDot
-	p2.Data = make([][]float64, 2)
-	p2.Data[0] = []float64{1, 2, 3, 4, 5}
-	p2.Data[1] = sinData[1][4:]
-	p2.SetRect(0, 15, 50, 30)
-	p2.AxesColor = ui.ColorWhite
-	p2.LineColors[0] = ui.ColorCyan
-	p2.PlotType = widgets.ScatterPlot
-
-	p3 = widgets.NewPlot()
-	p3.Title = "braille-mode Scatter Plot"
-	p3.Data = make([][]float64, 2)
-	p3.Data[0] = []float64{1, 2, 3, 4, 5}
-	p3.Data[1] = sinData[1][4:]
-	p3.SetRect(45, 15, 80, 30)
-	p3.AxesColor = ui.ColorWhite
-	p3.LineColors[0] = ui.ColorCyan
-	p3.Marker = widgets.MarkerBraille
-	p3.PlotType = widgets.ScatterPlot
+	p0.LineColors[1] = ui.ColorBlue
 
 	p = widgets.NewParagraph()
 	p.Text = "CMSC312 Operating System Simulator (press `q` to quit)"
@@ -128,10 +94,7 @@ func Render(k *kernel.Kernel) {
 			ui.NewCol(1.0/2, l0),
 		),
 		ui.NewRow(1.0/3,
-			ui.NewCol(1.0/4, p0),
-			ui.NewCol(1.0/4, p1),
-			ui.NewCol(1.0/4, p2),
-			ui.NewCol(1.0/4, p3),
+			ui.NewCol(1.0/1, p0),
 		),
 	)
 
@@ -162,6 +125,10 @@ func EventLoop() {
 			switch e.ID {
 			case "q", "<C-c>":
 				return
+			case "<Resize>":
+				payload := e.Payload.(ui.Resize)
+				grid.SetRect(0, 0, payload.Width, payload.Height)
+				ui.Clear()
 			}
 		}
 	}
